d30cd3b758c0f90aedcaa8661807037cb36da515
Aleks Anatoliyovich <44201618+Aleks0010V@users.noreply.github.com>
2020-10-13 11:00:25 +0300

    gracefull shutdown

--- main/main.go
+++ main/main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"CiklumGolangTask/modules"
+	"context"
 	"encoding/json"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	"sync"
+	"time"
 )
 
 var cmPosition = 5            // position of Content Marketing in pattern
@@ -54,13 +58,52 @@ func listApiHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func handleRequests() {
-	// ToDO - ideally, it should have graceful shutdown method
-	http.HandleFunc("/", listApiHandler)
+func handleRequests(ctx context.Context) (err error) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", listApiHandler)
+	srv := &http.Server{
+		Addr:    listeningString,
+		Handler: mux,
+	}
+	go func() {
+		if err = srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen:%s\n", err)
+		}
+	}()
 	log.Printf("STARTING API on port %s", listeningString[1:])
-	log.Fatal(http.ListenAndServe(listeningString, nil))
+	<-ctx.Done()
+	log.Print("STOPPING API")
+	ctxShutDown, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer func() {
+		cancel()
+	}()
+
+	if err = srv.Shutdown(ctxShutDown); err != nil {
+		log.Fatalf("server Shutdown Failed:%+s", err)
+	}
+
+	log.Printf("server exited properly")
+
+	if err == http.ErrServerClosed {
+		err = nil
+	}
+
+	return
 }
 
 func main() {
-	handleRequests()
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go func() {
+		oscall := <-c
+		log.Printf("system call:%+v", oscall)
+		cancel()
+	}()
+
+	if err := handleRequests(ctx); err != nil {
+		log.Printf("failed to serve:+%v\n", err)
+	}
 }
